c91252233e706b2e790eee33af54405f498d464e
Emil <somaleyoo123@gmail.com>
2025-05-13 12:03:51 +0200

    position fix

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,18 +1,20 @@
 import './profile.css'
 
-export default function Profile (props) {
+export default function Profile(props) {
+    return(
     <>
         <div className="profile-body">
             <div className="profile-info">
-                <img src={props.pfp} alt="" />
-                <p>Report for</p>
+                <img src={props.pfp} className='pfp' alt="" />
+                <p className='report'>Report for</p>
                 <h1 className='profile-name'>{props.name} </h1>
             </div>
             <div className="tracker">
-                <h3 className='daily'>Daily</h3>
-                <h3 className='weekly'>Weekly</h3>
-                <h3 className='monthly'>Monthly</h3>
+                <h3 className='daily track'>Daily</h3>
+                <h3 className='weekly track'>Weekly</h3>
+                <h3 className='monthly track'>Monthly</h3>
             </div>
         </div>
     </>
+    )
 }
